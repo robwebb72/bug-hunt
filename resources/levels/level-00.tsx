147f6a18a0e98f3f4775b8401bd8495c1986b09c
Rob <robstah72@gmail.com>
2020-07-14 08:45:22 +0100

    Update resource files

---
+++ resources/levels/level-00.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="bug_hunt_blocks" tilewidth="16" tileheight="16" tilecount="320" columns="20">
+ <image source="level-00-tiles.png" width="320" height="256"/>
+ <tile id="0" type="floor"/>
+ <tile id="1" type="floor"/>
+ <tile id="2" type="floor"/>
+ <tile id="3" type="floor"/>
+ <tile id="4" type="bullet_only"/>
+ <tile id="5" type="bullet_only"/>
+ <tile id="6" type="bullet_only"/>
+ <tile id="7" type="bullet_only"/>
+ <tile id="20" type="wall"/>
+ <tile id="21" type="wall"/>
+ <tile id="22" type="wall"/>
+ <tile id="23" type="wall"/>
+ <tile id="24" type="wall"/>
+ <tile id="25" type="wall"/>
+ <tile id="26" type="wall"/>
+ <tile id="27" type="wall"/>
+ <tile id="28" type="wall"/>
+ <tile id="29" type="wall"/>
+ <tile id="30" type="wall"/>
+ <tile id="31" type="wall"/>
+ <tile id="32" type="wall"/>
+ <tile id="33" type="wall"/>
+ <tile id="34" type="wall"/>
+ <tile id="40" type="floor"/>
+ <tile id="41" type="floor"/>
+ <tile id="42" type="floor"/>
+ <tile id="80" type="door">
+  <animation>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="86" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="81" type="door">
+  <animation>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="87" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="82" type="door"/>
+ <tile id="83" type="door"/>
+ <tile id="84" type="door"/>
+ <tile id="85" type="door"/>
+ <tile id="86" type="floor"/>
+ <tile id="87" type="floor"/>
+ <tile id="100" type="floor">
+  <animation>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="106" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="101" type="floor">
+  <animation>
+   <frame tileid="103" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="107" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="102" type="floor"/>
+ <tile id="103" type="floor"/>
+ <tile id="104" type="floor"/>
+ <tile id="105" type="floor"/>
+ <tile id="106" type="floor"/>
+ <tile id="107" type="floor"/>
+ <tile id="120" type="door,exit"/>
+ <tile id="121" type="door,exit"/>
+ <tile id="122" type="wall,generator"/>
+ <tile id="123" type="wall,generator"/>
+ <tile id="124" type="floor,generator"/>
+ <tile id="125" type="floor,generator"/>
+ <tile id="140" type="floor"/>
+ <tile id="141" type="floor"/>
+ <tile id="142" type="floor,generator"/>
+ <tile id="143" type="floor,generator"/>
+ <tile id="144" type="floor,generator"/>
+ <tile id="145" type="floor,generator"/>
+ <tile id="240" type="pu_health"/>
+ <tile id="241" type="pu_ammo"/>
+ <tile id="242" type="pu_life"/>
+ <tile id="243" type="pu_100c"/>
+ <tile id="244" type="pu_1000c"/>
+ <tile id="300" type="player_start"/>
+</tileset>
